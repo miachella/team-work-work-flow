dd2ed3c90f8887151cf5f4995c41c95bee29fe7a
Mathilde IACHELLA <mathilde.iachella@gmail.com>
2020-10-01 11:40:28 +0200

    Creation de component personnes

--- teamWorkWorkFlow/src/app/app.module.ts
+++ teamWorkWorkFlow/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListePersonnesComponent } from './liste-personnes/liste-personnes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListePersonnesComponent
   ],
   imports: [
     BrowserModule
---
+++ teamWorkWorkFlow/src/app/liste-personnes/liste-personnes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-personnes',
+  templateUrl: './liste-personnes.component.html',
+  styleUrls: ['./liste-personnes.component.css']
+})
+export class ListePersonnesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
